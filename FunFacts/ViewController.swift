abe019b0423c94bdf76070ff4ce81a6c44c3d43a
Matthew Spear <Matthewtimothyspear@googlemail.com>
2016-04-18 23:36:32 +0100

    added interface + main vc

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController
 {
+  @IBOutlet var funFactsLabel: UILabel!
+  @IBOutlet var funFactsButton: UIButton!
+  
+  let factBook = FactBook()
+  
   override func viewDidLoad()
   {
     super.viewDidLoad()
+    funFactsLabel.text = factBook.randomFact()
+  }
+  
+  override func preferredStatusBarStyle() -> UIStatusBarStyle
+  {
+    return .LightContent
+  }
+  
+  @IBAction func buttonPressed(sender: UIButton)
+  {
+    let randomColour = factBook.randomColor()
+    
+    view.backgroundColor = randomColour
+    funFactsButton.tintColor = randomColour
+    
+    funFactsLabel.text = factBook.randomFact()
   }
 }
 
